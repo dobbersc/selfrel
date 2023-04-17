140771eabd2f784b66335ad3fb10aa08898b7976
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-04-17 14:05:45 +0200

    Initial commit

---
+++ src/selfrel/__main__.py
@@ -0,0 +1,11 @@
+from importlib_resources import files
+
+data_text = files('selfrel.resources').joinpath('description.txt').read_text()
+
+
+def main() -> None:
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
