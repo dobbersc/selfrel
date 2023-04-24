510da9178739e3e4b0ab72057a3ef3ba8d419b53
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-04-24 14:45:56 +0200

    Enable garbage collection on error

--- src/selfrel/utils/garbage_collector.py
+++ src/selfrel/utils/garbage_collector.py
@@ -5,7 +5,12 @@ from collections.abc import Iterator
 
 @contextlib.contextmanager
 def disable_garbage_collector() -> Iterator[None]:
+    """Contextmanager to disable Python's generational garbage collection."""
     if gc.isenabled():
         gc.disable()
+        try:
+            yield
+        finally:
+            gc.enable()
+    else:
         yield
-        gc.enable()
