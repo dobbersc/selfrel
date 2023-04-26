d1a1c2ad3a15dedc0496230b974024b3df2d1981
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-04-26 12:49:35 +0200

    Add repr and logging printout

--- src/selfrel/predictor.py
+++ src/selfrel/predictor.py
@@ -17,13 +17,24 @@ _register_sentence_serializer()
 
 @ray.remote
 class Predictor:
-    def __init__(self, model_path: Union[str, Path], **kwargs: Any) -> None:
-        _register_sentence_serializer()
+    def __init__(self, model_path: Union[str, Path], index: Optional[int] = None, **kwargs: Any) -> None:
+        register_sentence_serializer()
+
+        if index is None:
+            print(f"Loading Flair model {model_path!r}")
+        else:
+            print(f"Loading Flair model {model_path!r} at index {index!r}")
 
         self._model_path: Union[str, Path] = model_path
         self._model: Classifier = Classifier.load(model_path)
+        self._index = index
         self._kwargs = kwargs
 
     def predict(self, sentences: Union[Sentence, list[Sentence]]) -> Union[Sentence, list[Sentence]]:
         self._model.predict(sentences, **self._kwargs)
         return sentences
+
+    def __repr__(self) -> str:
+        if self._index is None:
+            return f"{type(self).__name__}(model={self._model_path!r})"
+        return f"{type(self).__name__}(index={self._index!r}, model={self._model_path!r})"
