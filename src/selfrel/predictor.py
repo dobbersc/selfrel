b46a2453deb7cf181dd04b0e6e8c8170736724ac
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-04-26 17:52:01 +0200

    Implement utility function to initialize a predictor actor pool and a buffered map function

--- src/selfrel/predictor.py
+++ src/selfrel/predictor.py
@@ -1,14 +1,21 @@
+from collections.abc import Callable, Iterable
 from pathlib import Path
-from typing import Any, Optional, Union
+from typing import Any, Iterator, Optional, TypeVar, Union
 
+import more_itertools
 import ray
 from flair.data import Sentence
 from flair.nn import Classifier
+from ray.actor import ActorHandle
+from ray.util import ActorPool
 
 from selfrel.serialization import from_conllu, to_conllu
 
+__all__ = ["register_sentence_serializer", "Predictor", "initialize_predictor_pool", "buffered_map"]
 
-__all__ = ["register_sentence_serializer", "Predictor"]
+
+T = TypeVar("T")
+V = TypeVar("V")
 
 
 def register_sentence_serializer() -> None:
@@ -29,7 +36,7 @@ class Predictor:
             print(f"Loading Flair model {model_path!r} at index {index!r}")
 
         self._model_path: Union[str, Path] = model_path
-        self._model: Classifier = Classifier.load(model_path)
+        self._model: Classifier[Sentence] = Classifier.load(model_path)
         self._index = index
         self._kwargs = kwargs
 
@@ -41,3 +48,32 @@ class Predictor:
         if self._index is None:
             return f"{type(self).__name__}(model={self._model_path!r})"
         return f"{type(self).__name__}(index={self._index!r}, model={self._model_path!r})"
+
+
+def initialize_predictor_pool(
+    num_actors: int, actor_options: Optional[dict[str, Any]] = None, **predictor_kwargs: Any
+) -> ActorPool:
+    """
+    Initializes a `ray.util.ActorPool` with :class:`Predictor` actors.
+    :param num_actors: The number of Ray actors in the predictor pool
+    :param actor_options: An optional dictionary of options passed to the actor
+    :param predictor_kwargs: Keyword arguments are passed to the :class:`Predictor` init
+    :return: An actor pool of :class:`Predictor` actors.
+    """
+    predictor_kwargs.pop("index", None)
+    predictors: list[ActorHandle] = [
+        Predictor.options(**actor_options).remote(**predictor_kwargs, index=index)  # type: ignore[attr-defined]
+        if actor_options
+        else Predictor.remote(**predictor_kwargs)  # type: ignore[attr-defined]
+        for index in range(num_actors)
+    ]
+    return ActorPool(predictors)
+
+
+def buffered_map(
+    actor_pool: ActorPool, fn: Callable[[ActorHandle, V], T], values: Iterable[V], buffer_size: int
+) -> Iterator[T]:
+    """Buffered version of `ray.util.ActorPool`'s `map` function."""
+    value_buffer: Iterator[list[V]] = more_itertools.batched(values, n=buffer_size)
+    for buffered_values in value_buffer:
+        yield from actor_pool.map(fn, buffered_values)  # type: ignore[arg-type]
