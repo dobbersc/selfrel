6f3e43b21f336bb35c5e335cd0a09e376883cd73
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-06 00:18:38 +0200

    Add tests for predictor

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -1,15 +1,27 @@
-# ray.init()
-#
-# sentence = Sentence("Berlin is the capital of Germany.")
-# sentence2 = Sentence("Berlin is the capital of Germany.")
-# model_ = Classifier.load("flair/ner-english-large")
-#
-# model_.predict(sentence2)
-#
-# model_ref = ray.put(model_)
-#
-# a = Predictor.options(num_gpus=1).remote(model_ref, index=2)
-# new = ray.get(a.predict.remote(sentence))
-#
-# print(sentence2)
-# print(new)
+from collections.abc import Callable
+from typing import Union
+
+import pytest
+import ray
+from flair.data import Sentence
+from flair.nn import Classifier
+
+from selfrel.predictor import Predictor
+
+
+@pytest.mark.parametrize(
+    "model_factory",
+    [lambda: "flair/ner-english-fast", lambda: Classifier.load("flair/ner-english-fast")],
+    ids=["from_path", "from_instance"],
+)
+def test_predictor(model_factory: Callable[[], Union[str, Classifier]], init_ray: None) -> None:
+    sentence = Sentence("Berlin is the capital of Germany.")
+
+    model_ref = ray.put(model_factory())
+    predictor = Predictor.remote(model_ref, index=1, label_name="prediction")
+
+    result: Sentence = ray.get(predictor.predict.remote(sentence))
+    assert {(entity.text, entity.get_label("prediction").value) for entity in result.get_spans("prediction")} == {
+        ("Berlin", "LOC"),
+        ("Germany", "LOC"),
+    }
