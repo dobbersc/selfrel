98fcd9c5bbc541883712546209885827883bef36
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-05 16:21:17 +0200

    Refactor annotate entry point to automatically detect the label type and abstraction level

--- src/selfrel/entry_points/annotate.py
+++ src/selfrel/entry_points/annotate.py
@@ -6,6 +6,8 @@ from typing import Literal, Optional, Union
 import more_itertools
 import ray
 from flair.data import Sentence
+from flair.models import SequenceTagger, TextClassifier, RelationClassifier
+from flair.nn import Classifier
 from ray.actor import ActorHandle
 from ray.util import ActorPool
 from tqdm import tqdm
@@ -16,13 +18,38 @@ from selfrel.data.serialization import to_conllu
 
 __all__ = ["annotate"]
 
+AbstractionLevel = Literal["token", "span", "relation", "sentence"]
+
+
+def _infer_abstraction_level(classifier: Classifier) -> AbstractionLevel:
+    if isinstance(classifier, SequenceTagger):
+        # noinspection PyTypeChecker
+        return "span" if classifier.predict_spans else "token"
+
+    if isinstance(classifier, RelationClassifier):
+        return "relation"
+
+    if isinstance(classifier, TextClassifier):
+        return "sentence"
+
+    raise ValueError(f"Received unsupported classifier of type {type(classifier).__name__}")
+
+
+def _get_sentence_serializer(abstraction_level: AbstractionLevel, label_type: str) -> functools.partial[str]:
+    if abstraction_level == "token":
+        return functools.partial(to_conllu, default_token_fields={label_type})
+    if abstraction_level == "span":
+        return functools.partial(to_conllu, default_span_fields={label_type})
+    # relation and sentence
+    return functools.partial(to_conllu)
+
 
 def annotate(
     dataset_path: Union[str, Path],
     out_path: Union[str, Path] = None,
     model_path: str = "flair/ner-english-large",
-    label_type: str = "ner",
-    abstraction_level: Literal["token", "span", "relation", "sentence"] = "span",
+    label_type: Optional[str] = None,
+    abstraction_level: Optional[AbstractionLevel] = None,
     batch_size: int = 32,
     num_actors: int = 1,
     num_cpus: Optional[float] = None,
@@ -30,26 +57,22 @@ def annotate(
     buffer_size: Optional[int] = None,
 ) -> None:
     """See `selfrel annotate --help`."""
-    dataset_path = Path(dataset_path)
-
-    # Set default output path
-    if out_path is None:
-        out_path = dataset_path.parent / f"{dataset_path.stem}-{label_type}{dataset_path.suffix}"
-    out_path = Path(out_path)
-
-    # Create output directory
-    out_path.parent.mkdir(parents=True, exist_ok=True)
-
     # Set default buffer size
     buffer_size = num_actors if buffer_size is None else 2 * buffer_size
 
     # Initialize ray cluster
     ray.init()
 
+    # Load Flair classifier
+    classifier: Classifier = Classifier.load(model_path)
+    classifier_ref: ray.ObjectRef = ray.put(classifier)
+
+    label_type = classifier.label_type if label_type is None else label_type
+
     # Initialize predictor actor pool
     predictor_pool: ActorPool = initialize_predictor_pool(
         num_actors,
-        model_path=model_path,
+        model=classifier_ref,
         label_name=label_type,
         mini_batch_size=batch_size,
         actor_options={"num_cpus": num_cpus, "num_gpus": num_gpus},
@@ -57,6 +80,15 @@ def annotate(
 
     def remote_predict(pipeline_actor: ActorHandle, sentences: list[Sentence]) -> list[Sentence]:
         return pipeline_actor.predict.remote(sentences)  # type: ignore[no-any-return]
+
+    # Set dataset and output path
+    dataset_path = Path(dataset_path)
+    if out_path is None:
+        out_path = dataset_path.parent / f"{dataset_path.stem}-{label_type}{dataset_path.suffix}"
+    out_path = Path(out_path)
+
+    # Create output directory
+    out_path.parent.mkdir(parents=True, exist_ok=True)
 
     # Load dataset
     dataset: CoNLLUPlusDataset = CoNLLUPlusDataset(dataset_path, persist=False)
@@ -66,17 +98,11 @@ def annotate(
     )
 
     # Set serialization function based on abstraction level
-    to_conllu_partial: functools.partial[str]
-    if abstraction_level == "token":
-        to_conllu_partial = functools.partial(to_conllu, default_token_fields={label_type})
-    elif abstraction_level == "span":
-        to_conllu_partial = functools.partial(to_conllu, default_span_fields={label_type})
-    else:
-        # relation and sentence
-        to_conllu_partial = functools.partial(to_conllu)
+    abstraction_level = _infer_abstraction_level(classifier) if abstraction_level is None else abstraction_level
+    sentence_to_conllu: functools.partial[str] = _get_sentence_serializer(abstraction_level, label_type)
 
     # Get global.columns including the new label type
-    global_columns: str = to_conllu_partial(dataset[0]).split("\n", 1)[0]
+    global_columns: str = sentence_to_conllu(dataset[0]).split("\n", 1)[0]
 
     # Process dataset
     with out_path.open("w", encoding="utf-8") as output_file:
@@ -87,7 +113,7 @@ def annotate(
                 predictor_pool, fn=remote_predict, values=sentence_batches, buffer_size=buffer_size
             ):
                 output_file.writelines(
-                    to_conllu_partial(processed_sentence, include_global_columns=False)
+                    sentence_to_conllu(processed_sentence, include_global_columns=False)
                     for processed_sentence in processed_sentences
                 )
                 progress_bar.update(len(processed_sentences))
