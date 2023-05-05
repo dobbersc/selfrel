860a7d2a1cf91e6ad2617c503a4fbce3018361da
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-05 15:26:51 +0200

    Allow classifier model and path as predictor initializer

--- src/selfrel/predictor.py
+++ src/selfrel/predictor.py
@@ -1,6 +1,6 @@
 from collections.abc import Callable, Iterable
 from pathlib import Path
-from typing import Any, Iterator, Optional, TypeVar, Union
+from typing import Any, Iterator, Optional, TypeVar, Union, overload
 
 import more_itertools
 import ray
@@ -17,6 +17,8 @@ __all__ = ["register_sentence_serializer", "Predictor", "initialize_predictor_po
 T = TypeVar("T")
 V = TypeVar("V")
 
+SentenceT = TypeVar("SentenceT", bound=Sentence)
+
 
 def register_sentence_serializer() -> None:
     ray.util.register_serializer(Sentence, serializer=to_conllu, deserializer=from_conllu)
@@ -27,20 +29,39 @@ register_sentence_serializer()
 
 @ray.remote
 class Predictor:
-    def __init__(self, model_path: Union[str, Path], index: Optional[int] = None, **kwargs: Any) -> None:
+    """A Ray actor that wraps the predict function of Flair's Classifier model."""
+
+    def __init__(self, model: Union[str, Path, Classifier], index: Optional[int] = None, **kwargs: Any) -> None:
+        """
+        Initializes a :class:`Predictor` as Ray actor from a Flair classifier.
+        :param model: A model path or identifier for a Flair classifier.
+                      If providing a Flair classifier, it is recommended to put in into Ray's object store.
+                      Reference: https://docs.ray.io/en/latest/ray-core/objects.html.
+        :param index: An optional index that is attached to the predictor for logging purposes.
+        :param kwargs: Keywords arguments are passed to the classifier's predict function.
+        """
         register_sentence_serializer()
 
-        if index is None:
-            print(f"Loading Flair model {model_path!r}")
-        else:
-            print(f"Loading Flair model {model_path!r} at index {index!r}")
-
-        self._model_path: Union[str, Path] = model_path
-        self._model: Classifier[Sentence] = Classifier.load(model_path)
+        self._model = self._model if isinstance(model, Classifier) else Classifier.load(model)
         self._index = index
         self._kwargs = kwargs
 
-    def predict(self, sentences: Union[Sentence, list[Sentence]]) -> Union[Sentence, list[Sentence]]:
+        model_message: str = "from instance" if isinstance(model, Classifier) else f"from {model!r}"
+        index_message: str = "" if index is None else f"at index {self._index!r} "
+        print(
+            f"Loaded Flair model {model_message} as {type(self._model).__name__!r} "
+            f"predicting labels of label type {self._model.label_type!r}{index_message}"
+        )
+
+    @overload
+    def predict(self, sentences: SentenceT) -> SentenceT:
+        ...
+
+    @overload
+    def predict(self, sentences: SentenceT) -> list[SentenceT]:
+        ...
+
+    def predict(self, sentences: Union[SentenceT, list[SentenceT]]) -> Union[SentenceT, list[SentenceT]]:
         self._model.predict(sentences, **self._kwargs)
         return sentences
 
