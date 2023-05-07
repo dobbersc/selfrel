5acccb9c90377a638f4e9801c7419dd164c7ea8f
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-07 20:12:13 +0200

    Add test for predictor pool

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -5,8 +5,9 @@ import pytest
 import ray
 from flair.data import Sentence
 from flair.nn import Classifier
+from ray.actor import ActorHandle
 
-from selfrel.predictor import Predictor
+from selfrel.predictor import Predictor, buffered_map, initialize_predictor_pool
 
 
 @pytest.mark.usefixtures("_init_ray")
@@ -26,3 +27,30 @@ def test_predictor(model_factory: Callable[[], Union[str, Classifier[Sentence]]]
         ("Berlin", "LOC"),
         ("Germany", "LOC"),
     }
+
+
+@pytest.mark.usefixtures("_init_ray")
+@pytest.mark.parametrize(
+    "model_factory",
+    [lambda: "flair/ner-english-fast", lambda: Classifier.load("flair/ner-english-fast")],
+    ids=["from_path", "from_instance"],
+)
+def test_predictor_pool(model_factory: Callable[[], Union[str, Classifier[Sentence]]]) -> None:
+    sentence = Sentence("Berlin is the capital of Germany.")
+
+    # Set-up predictor pool
+    model_ref = ray.put(model_factory())
+    predictor_pool = initialize_predictor_pool(2, model=model_ref, label_name="prediction")
+
+    def remote_predict(pipeline_actor: ActorHandle, sentence_: Sentence) -> Sentence:
+        return pipeline_actor.predict.remote(sentence_)  # type: ignore[no-any-return]
+
+    # Test buffered_map
+    result_sentences = list(buffered_map(predictor_pool, fn=remote_predict, values=[sentence], buffer_size=1))
+    assert len(result_sentences) == 1
+    result_sentence = result_sentences[0]
+
+    expected: set[tuple[str, str]] = {
+        (entity.text, entity.get_label("prediction").value) for entity in result_sentence.get_spans("prediction")
+    }
+    assert expected == {("Berlin", "LOC"), ("Germany", "LOC")}
