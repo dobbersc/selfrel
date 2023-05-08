07ba6a2eff833028ea587f2509dd2bfe817f4dc8
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-08 21:20:24 +0200

    Implement function to deepcopy flair models

---
+++ src/selfrel/utils/copy.py
@@ -0,0 +1,20 @@
+import tempfile
+from pathlib import Path
+from typing import TypeVar
+
+from flair.nn import Model
+
+ModelT = TypeVar("ModelT", bound=Model)
+
+
+def deepcopy_flair_model(model: ModelT, copy_optimizer_state: bool = False) -> ModelT:
+    with tempfile.NamedTemporaryFile(suffix=".pt", delete=False) as file:
+        model_path: Path = Path(file.name)
+
+    try:
+        model.save(model_path, checkpoint=copy_optimizer_state)
+        model_copy: ModelT = model.load(model_path)
+    finally:
+        model_path.unlink()
+
+    return model_copy
