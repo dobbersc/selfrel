393ac4a5bcc6d5620a74a035f4b7560a70542b50
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-10 17:52:42 +0200

    Adds option to disable the progress bar and add custom dataset name

--- src/selfrel/data/conllu.py
+++ src/selfrel/data/conllu.py
@@ -1,7 +1,7 @@
 import os
 from collections.abc import Iterator, Sized
 from pathlib import Path
-from typing import Any, TextIO, Union, cast, overload
+from typing import Any, Optional, TextIO, Union, cast, overload
 
 from flair.data import Sentence
 from joblib import Parallel, delayed
@@ -13,13 +13,17 @@ from selfrel.data.serialization import from_conllu
 __all__ = ["CoNLLUPlusDataset"]
 
 
-def _serialized_conllu_plus_sentence_iter(fp: TextIO, disable_progress_bar: bool = False) -> Iterator[str]:
+def _serialized_conllu_plus_sentence_iter(
+    fp: TextIO,
+    dataset_name: str,
+    disable_progress_bar: bool = False,
+) -> Iterator[str]:
     dataset_size: int = os.fstat(fp.fileno()).st_size
     with tqdm(
         total=dataset_size,
         unit="B",
         unit_scale=True,
-        desc="Loading Dataset",
+        desc=f"Loading Dataset {dataset_name!r}",
         disable=disable_progress_bar,
     ) as progress_bar:
         global_columns: str = fp.readline()
@@ -44,6 +48,7 @@ def _serialized_conllu_plus_sentence_iter(fp: TextIO, disable_progress_bar: bool
 def _parse_conllu_plus(
     fp: TextIO,
     processes: int,
+    dataset_name: str,
     disable_progress_bar: bool = False,
     **kwargs: Any,
 ) -> Iterator[Sentence]:
@@ -53,7 +58,9 @@ def _parse_conllu_plus(
     parallel = Parallel(processes, return_generator=True, **kwargs)
     sentences: Iterator[Sentence] = parallel(
         delayed(from_conllu)(sentence)
-        for sentence in _serialized_conllu_plus_sentence_iter(fp, disable_progress_bar=disable_progress_bar)
+        for sentence in _serialized_conllu_plus_sentence_iter(
+            fp, dataset_name, disable_progress_bar=disable_progress_bar
+        )
     )
     return sentences
 
@@ -66,10 +73,19 @@ class CoNLLUPlusDataset(Dataset[Sentence], Sized):
     - Universal Dependencies CoNLL-U Plus format: https://universaldependencies.org/ext-format.html
     """
 
-    def __init__(self, dataset_path: Union[str, Path], processes: int = 1, persist: bool = True, **kwargs: Any) -> None:
+    def __init__(
+        self,
+        dataset_path: Union[str, Path],
+        dataset_name: Optional[str] = None,
+        processes: int = 1,
+        persist: bool = True,
+        disable_progress_bar: bool = False,
+        **kwargs: Any,
+    ) -> None:
         """
         Initializes a :class:`CoNLLUPlusDataset` from a CoNLL-U Plus file.
         :param dataset_path: The path to the dataset file
+        :param dataset_name: A name for the dataset. Per default, the dataset's path is used.
         :param processes: The number of processes to parse the dataset file
         :param persist: If True, the entire dataset will be converted to Flair Sentences
                         immediately upon initialization and loaded into memory.
@@ -78,10 +94,12 @@ class CoNLLUPlusDataset(Dataset[Sentence], Sized):
                         If False, only the dataset file is loaded into memory as a string.
                         Calls to `__getitem__` or `__iter__` parse the dataset file on the fly to Flair Sentences.
                         The returned Sentences are not persistent.
+        :param disable_progress_bar: Disables the progress bar for loading the dataset
         :param kwargs: Keyword arguments are passed down to the internal use of joblib.
                        Parameters `n_jobs` and `return_generator` will be ignored.
         """
         self._dataset_path = Path(dataset_path)
+        self._dataset_name = str(self._dataset_path) if dataset_name is None else dataset_name
         self._processes = processes
         self._persist = persist
         self._kwargs = kwargs
@@ -89,9 +107,13 @@ class CoNLLUPlusDataset(Dataset[Sentence], Sized):
         with self.dataset_path.open("r", encoding="utf-8") as dataset_file:
             self._sentences: Union[tuple[Sentence, ...], tuple[str, ...]]
             if self.is_persistent:
-                self._sentences = tuple(_parse_conllu_plus(dataset_file, processes, **kwargs))
+                self._sentences = tuple(
+                    _parse_conllu_plus(dataset_file, processes, dataset_name, disable_progress_bar, **kwargs)
+                )
             else:
-                self._sentences = tuple(_serialized_conllu_plus_sentence_iter(dataset_file))
+                self._sentences = tuple(
+                    _serialized_conllu_plus_sentence_iter(dataset_file, dataset_name, disable_progress_bar)
+                )
 
         if not self._sentences:
             msg = "Empty datasets are not supported"
@@ -100,6 +122,10 @@ class CoNLLUPlusDataset(Dataset[Sentence], Sized):
     @property
     def dataset_path(self) -> Path:
         return self._dataset_path
+
+    @property
+    def dataset_name(self) -> str:
+        return self._dataset_name
 
     @property
     def is_persistent(self) -> bool:
@@ -141,3 +167,10 @@ class CoNLLUPlusDataset(Dataset[Sentence], Sized):
 
     def __len__(self) -> int:
         return len(self._sentences)
+
+    def __repr__(self) -> str:
+        attributes: list[tuple[str, str]] = [("dataset_path", repr(self._dataset_path))]
+        if self._dataset_name != str(self._dataset_path):
+            attributes.append(("dataset_name", repr(self._dataset_name)))
+        attributes.append(("persist", repr(self._persist)))
+        return f"{type(self).__name__}({', '.join(f'{attribute}={value}' for attribute, value in attributes)})"
