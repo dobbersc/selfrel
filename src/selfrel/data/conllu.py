5863ea9419e3e5547f45d16f2368c6bd5dc272c9
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-12 19:17:57 +0200

    Remove parallel computing from `CoNLLUPlusDataset` as it does not work on sentences with span annotations

--- src/selfrel/data/conllu.py
+++ src/selfrel/data/conllu.py
@@ -1,10 +1,9 @@
 import os
 from collections.abc import Iterator, Sized
 from pathlib import Path
-from typing import Any, Optional, TextIO, Union, cast, overload
+from typing import Optional, TextIO, Union, cast, overload
 
 from flair.data import Sentence
-from joblib import Parallel, delayed
 from torch.utils.data.dataset import Dataset
 from tqdm import tqdm
 
@@ -50,26 +49,6 @@ def _serialized_conllu_plus_sentence_iter(
             yield f"{global_columns}{sentence}"
 
 
-def _parse_conllu_plus(
-    fp: TextIO,
-    processes: int,
-    dataset_name: str,
-    disable_progress_bar: bool = False,
-    **kwargs: Any,
-) -> Iterator[Sentence]:
-    kwargs.pop("n_jobs", None)
-    kwargs.pop("return_generator", None)
-
-    parallel = Parallel(processes, return_generator=True, **kwargs)
-    sentences: Iterator[Sentence] = parallel(
-        delayed(from_conllu)(sentence)
-        for sentence in _serialized_conllu_plus_sentence_iter(
-            fp, dataset_name, disable_progress_bar=disable_progress_bar
-        )
-    )
-    return sentences
-
-
 class CoNLLUPlusDataset(Dataset[Sentence], Sized):
     """
     Dataset of Flair Sentences parsed from a CoNLL-U Plus file.
@@ -82,16 +61,13 @@ class CoNLLUPlusDataset(Dataset[Sentence], Sized):
         self,
         dataset_path: Union[str, Path],
         dataset_name: Optional[str] = None,
-        processes: int = 1,
         persist: bool = True,
         disable_progress_bar: bool = False,
-        **kwargs: Any,
     ) -> None:
         """
         Initializes a :class:`CoNLLUPlusDataset` from a CoNLL-U Plus file.
         :param dataset_path: The path to the dataset file
         :param dataset_name: A name for the dataset. Per default, the dataset's path is used.
-        :param processes: The number of processes to parse the dataset file
         :param persist: If True, the entire dataset will be converted to Flair Sentences
                         immediately upon initialization and loaded into memory.
                         If the dataset is very large, temporarily disabling Python's generational garbage collector
@@ -100,20 +76,19 @@ class CoNLLUPlusDataset(Dataset[Sentence], Sized):
                         Calls to `__getitem__` or `__iter__` parse the dataset file on the fly to Flair Sentences.
                         The returned Sentences are not persistent.
         :param disable_progress_bar: Disables the progress bar for loading the dataset
-        :param kwargs: Keyword arguments are passed down to the internal use of joblib.
-                       Parameters `n_jobs` and `return_generator` will be ignored.
         """
         self._dataset_path = Path(dataset_path)
         self._dataset_name = str(self._dataset_path) if dataset_name is None else dataset_name
-        self._processes = processes
         self._persist = persist
-        self._kwargs = kwargs
 
         with self.dataset_path.open("r", encoding="utf-8") as dataset_file:
             self._sentences: Union[tuple[Sentence, ...], tuple[str, ...]]
             if self.is_persistent:
                 self._sentences = tuple(
-                    _parse_conllu_plus(dataset_file, processes, self._dataset_name, disable_progress_bar, **kwargs)
+                    from_conllu(sentence)
+                    for sentence in _serialized_conllu_plus_sentence_iter(
+                        dataset_file, self._dataset_name, disable_progress_bar
+                    )
                 )
             else:
                 self._sentences = tuple(
@@ -162,15 +137,8 @@ class CoNLLUPlusDataset(Dataset[Sentence], Sized):
             assert isinstance(self._sentences[0], Sentence)
             yield from cast(tuple[Sentence, ...], self._sentences)
         else:
-            with self._dataset_path.open("r", encoding="utf-8") as dataset_file:
-                yield from _parse_conllu_plus(
-                    dataset_file,
-                    self._processes,
-                    self._dataset_name,
-                    disable_progress_bar=True,
-                    return_generator=True,
-                    **self._kwargs,
-                )
+            for sentence in self._sentences:
+                yield from_conllu(sentence)
 
     def __len__(self) -> int:
         return len(self._sentences)
