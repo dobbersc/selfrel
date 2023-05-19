3e442cf0a44636ee1ffe4c65404919bc64f5c8a9
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-19 17:27:52 +0200

    Move selected sentence creation into a separate function

--- src/selfrel/selection_strategies.py
+++ src/selfrel/selection_strategies.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from collections.abc import Iterator, Sequence
 
-from flair.data import Relation, Sentence
+from flair.data import Label, Relation, Sentence
 
 from selfrel.utils.copy import deepcopy_flair_sentence
 
@@ -9,6 +9,21 @@ __all__ = ["SelectionStrategy", "PredictionConfidence", "TotalOccurrence", "PMI"
 
 
 class SelectionStrategy(ABC):
+    @staticmethod
+    def _create_selected_sentence(sentence: Sentence, relations: list[Relation], label_type: str) -> Sentence:
+        sentence = deepcopy_flair_sentence(sentence)
+        sentence.remove_labels(label_type)  # Remove relation annotations
+
+        for relation in relations:
+            label: Label = relation.get_label(label_type)
+            selected_relation: Relation = Relation(
+                first=sentence[relation.first[0].idx - 1 : relation.first[-1].idx],
+                second=sentence[relation.second[0].idx - 1 : relation.second[-1].idx],
+            )
+            selected_relation.add_label(label_type, value=label.value, score=label.score)
+
+        return sentence
+
     @abstractmethod
     def select_relations(self, sentences: Sequence[Sentence], label_type: str) -> Iterator[Sentence]:
         pass
@@ -20,21 +35,15 @@ class PredictionConfidence(SelectionStrategy):
 
     def select_relations(self, sentences: Sequence[Sentence], label_type: str) -> Iterator[Sentence]:
         for sentence in sentences:
-            selected_sentence: Sentence = deepcopy_flair_sentence(sentence)
-            selected_sentence.remove_labels(label_type)  # Remove relation annotations
-
-            selected_relations: list[Relation] = []
-            for relation in sentence.get_relations(label_type):
-                if (label := relation.get_label(label_type)).score >= self.confidence_threshold:
-                    selected_relation: Relation = Relation(
-                        first=selected_sentence[relation.first[0].idx - 1 : relation.first[-1].idx],
-                        second=selected_sentence[relation.second[0].idx - 1 : relation.second[-1].idx],
-                    )
-                    selected_relation.add_label(label_type, value=label.value, score=label.score)
-                    selected_relations.append(selected_relation)
-
+            selected_relations: list[Relation] = [
+                relation
+                for relation in sentence.get_relations(label_type)
+                if relation.get_label(label_type).score >= self.confidence_threshold
+            ]
             if selected_relations:
-                yield selected_sentence
+                yield self._create_selected_sentence(
+                    sentence=sentence, relations=selected_relations, label_type=label_type
+                )
 
 
 class TotalOccurrence(SelectionStrategy):
