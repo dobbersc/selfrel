93b6202a73e56dc574c8760646149105622bae98
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-25 12:02:39 +0200

    Refactor tests

--- tests/entry_points/export/test_knowledge_base.py
+++ tests/entry_points/export/test_knowledge_base.py
@@ -60,95 +60,97 @@ def knowledge_base(dataset: list[Sentence], tmp_path_factory: TempPathFactory) -
     connection.close()
 
 
-class TestTables:
-    def test_sentences(self, knowledge_base: sqlite3.Cursor) -> None:
-        assert knowledge_base.execute("SELECT * FROM sentences ORDER BY sentence_id").fetchall() == [
-            (1, "Berlin is the capital of Germany."),
-            (2, "Albert Einstein was born in Ulm, Germany."),
-            (3, "Ulm, located in Germany, is the birthplace of Albert Einstein."),
-            (4, "This is a sentence."),
-        ]
-
-    def test_entities(self, knowledge_base: sqlite3.Cursor) -> None:
-        assert knowledge_base.execute("SELECT * FROM entities ORDER BY entity_id").fetchall() == [
-            (1, "Berlin", "LOC"),
-            (2, "Germany", "LOC"),
-            (3, "Albert Einstein", "PER"),
-            (4, "Ulm", "LOC"),
-        ]
-
-    def test_relations(self, knowledge_base: sqlite3.Cursor) -> None:
-        assert knowledge_base.execute("SELECT * FROM relations ORDER BY relation_id").fetchall() == [
-            (1, 1, 2, "capital_of"),  # Berlin -> Germany
-            (2, 3, 4, "born_in"),  # Albert Einstein -> Ulm
-            (3, 4, 2, "located_in"),  # Ulm -> Germany
-        ]
-
-    def test_sentence_entities(self, knowledge_base: sqlite3.Cursor) -> None:
-        assert knowledge_base.execute("SELECT * FROM sentence_entities ORDER BY sentence_entity_id").fetchall() == [
-            # Sentence 1
-            (1, 1, 1, 1.0),  # Berlin
-            (2, 1, 2, 1.0),  # Germany
-            # Sentence 2
-            (3, 2, 3, 1.0),  # Albert Einstein
-            (4, 2, 4, 1.0),  # Ulm
-            (5, 2, 2, 1.0),  # Germany
-            # Sentence 3
-            (6, 3, 4, 1.0),  # Ulm
-            (7, 3, 2, 1.0),  # Germany
-            (8, 3, 3, 1.0),  # Albert Einstein
-        ]
-
-    def test_sentence_relations(self, knowledge_base: sqlite3.Cursor) -> None:
-        assert knowledge_base.execute("SELECT * FROM sentence_relations ORDER BY sentence_relation_id").fetchall() == [
-            # Sentence 1
-            (1, 1, 1, 1.0),  # Berlin -> Germany
-            # Sentence 2
-            (2, 2, 2, 1.0),  # Albert Einstein -> Ulm
-            (3, 2, 3, 1.0),  # Ulm -> Germany
-            # Sentence 3
-            (4, 3, 2, 1.0),  # Albert Einstein -> Ulm
-            (5, 3, 3, 1.0),  # Ulm -> Germany
-        ]
+def test_sentences(knowledge_base: sqlite3.Cursor) -> None:
+    assert knowledge_base.execute("SELECT * FROM sentences ORDER BY sentence_id").fetchall() == [
+        (1, "Berlin is the capital of Germany."),
+        (2, "Albert Einstein was born in Ulm, Germany."),
+        (3, "Ulm, located in Germany, is the birthplace of Albert Einstein."),
+        (4, "This is a sentence."),
+    ]
 
 
-class TestViews:
-    def test_relation_overview(self, knowledge_base: sqlite3.Cursor) -> None:
-        # | sentence_relation_id |sentence_id | relation_id | head_id | tail_id
-        # | sentence_text |
-        # | head_text | tail_text | head_label | tail_label | label
-        # | confidence | occurrence
-        # fmt: off
-        assert knowledge_base.execute("SELECT * FROM relation_overview ORDER BY sentence_relation_id").fetchall() == [
-            (
-                1, 1, 1, 1, 2,
-                "Berlin is the capital of Germany.",
-                "Berlin", "Germany", "LOC", "LOC", "capital_of",
-                1.0, 1,
-            ),
-            (
-                2, 2, 2, 3, 4,
-                "Albert Einstein was born in Ulm, Germany.",
-                "Albert Einstein", "Ulm", "PER", "LOC", "born_in",
-                1.0, 2,
-            ),
-            (
-                3, 2, 3, 4, 2,
-                "Albert Einstein was born in Ulm, Germany.",
-                "Ulm", "Germany", "LOC", "LOC", "located_in",
-                1.0, 2,
-            ),
-            (
-                4, 3, 2, 3, 4,
-                "Ulm, located in Germany, is the birthplace of Albert Einstein.",
-                "Albert Einstein", "Ulm", "PER", "LOC", "born_in",
-                1.0, 2,
-            ),
-            (
-                5, 3, 3, 4, 2,
-                "Ulm, located in Germany, is the birthplace of Albert Einstein.",
-                "Ulm", "Germany", "LOC", "LOC", "located_in",
-                1.0, 2,
-            ),
-        ]
-        # fmt: on
+def test_entities(knowledge_base: sqlite3.Cursor) -> None:
+    assert knowledge_base.execute("SELECT * FROM entities ORDER BY entity_id").fetchall() == [
+        (1, "Berlin", "LOC"),
+        (2, "Germany", "LOC"),
+        (3, "Albert Einstein", "PER"),
+        (4, "Ulm", "LOC"),
+    ]
+
+
+def test_relations(knowledge_base: sqlite3.Cursor) -> None:
+    assert knowledge_base.execute("SELECT * FROM relations ORDER BY relation_id").fetchall() == [
+        (1, 1, 2, "capital_of"),  # Berlin -> Germany
+        (2, 3, 4, "born_in"),  # Albert Einstein -> Ulm
+        (3, 4, 2, "located_in"),  # Ulm -> Germany
+    ]
+
+
+def test_sentence_entities(knowledge_base: sqlite3.Cursor) -> None:
+    assert knowledge_base.execute("SELECT * FROM sentence_entities ORDER BY sentence_entity_id").fetchall() == [
+        # Sentence 1
+        (1, 1, 1, 1.0),  # Berlin
+        (2, 1, 2, 1.0),  # Germany
+        # Sentence 2
+        (3, 2, 3, 1.0),  # Albert Einstein
+        (4, 2, 4, 1.0),  # Ulm
+        (5, 2, 2, 1.0),  # Germany
+        # Sentence 3
+        (6, 3, 4, 1.0),  # Ulm
+        (7, 3, 2, 1.0),  # Germany
+        (8, 3, 3, 1.0),  # Albert Einstein
+    ]
+
+
+def test_sentence_relations(knowledge_base: sqlite3.Cursor) -> None:
+    assert knowledge_base.execute("SELECT * FROM sentence_relations ORDER BY sentence_relation_id").fetchall() == [
+        # Sentence 1
+        (1, 1, 1, 1.0),  # Berlin -> Germany
+        # Sentence 2
+        (2, 2, 2, 1.0),  # Albert Einstein -> Ulm
+        (3, 2, 3, 1.0),  # Ulm -> Germany
+        # Sentence 3
+        (4, 3, 2, 1.0),  # Albert Einstein -> Ulm
+        (5, 3, 3, 1.0),  # Ulm -> Germany
+    ]
+
+
+def test_relation_overview(knowledge_base: sqlite3.Cursor) -> None:
+    # | sentence_relation_id |sentence_id | relation_id | head_id | tail_id
+    # | sentence_text |
+    # | head_text | tail_text | head_label | tail_label | label
+    # | confidence | occurrence
+    # fmt: off
+    assert knowledge_base.execute("SELECT * FROM relation_overview ORDER BY sentence_relation_id").fetchall() == [
+        (
+            1, 1, 1, 1, 2,
+            "Berlin is the capital of Germany.",
+            "Berlin", "Germany", "LOC", "LOC", "capital_of",
+            1.0, 1,
+        ),
+        (
+            2, 2, 2, 3, 4,
+            "Albert Einstein was born in Ulm, Germany.",
+            "Albert Einstein", "Ulm", "PER", "LOC", "born_in",
+            1.0, 2,
+        ),
+        (
+            3, 2, 3, 4, 2,
+            "Albert Einstein was born in Ulm, Germany.",
+            "Ulm", "Germany", "LOC", "LOC", "located_in",
+            1.0, 2,
+        ),
+        (
+            4, 3, 2, 3, 4,
+            "Ulm, located in Germany, is the birthplace of Albert Einstein.",
+            "Albert Einstein", "Ulm", "PER", "LOC", "born_in",
+            1.0, 2,
+        ),
+        (
+            5, 3, 3, 4, 2,
+            "Ulm, located in Germany, is the birthplace of Albert Einstein.",
+            "Ulm", "Germany", "LOC", "LOC", "located_in",
+            1.0, 2,
+        ),
+    ]
+    # fmt: on
