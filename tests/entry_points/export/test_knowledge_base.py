84b190bf3a805f2e03c4ad8a466d92c71bbc395b
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-27 15:00:57 +0200

    Refactor test structure

--- tests/entry_points/export/test_knowledge_base.py
+++ tests/entry_points/export/test_knowledge_base.py
@@ -69,84 +69,96 @@ def knowledge_base(dataset: list[Sentence], tmp_path_factory: TempPathFactory) -
     connection.close()
 
 
-def test_sentences(knowledge_base: sqlite3.Cursor) -> None:
-    assert knowledge_base.execute("SELECT * FROM sentences ORDER BY sentence_id").fetchall() == [
-        (1, "Berlin is the capital of Germany."),
-        (2, "Berlin is the capital of Germany."),
-        (3, "Albert Einstein was born in Ulm, Germany."),
-        (4, "Ulm, located in Germany, is the birthplace of Albert Einstein."),
-        (5, "Amazon was founded by Jeff Bezos."),
-        (6, "This is a sentence."),
-    ]
+class TestTables:
+    def test_sentences(self, knowledge_base: sqlite3.Cursor) -> None:
+        assert knowledge_base.execute("SELECT * FROM sentences ORDER BY sentence_id").fetchall() == [
+            (1, "Berlin is the capital of Germany."),
+            (2, "Berlin is the capital of Germany."),
+            (3, "Albert Einstein was born in Ulm, Germany."),
+            (4, "Ulm, located in Germany, is the birthplace of Albert Einstein."),
+            (5, "Amazon was founded by Jeff Bezos."),
+            (6, "This is a sentence."),
+        ]
+
+    def test_entities(self, knowledge_base: sqlite3.Cursor) -> None:
+        assert knowledge_base.execute("SELECT * FROM entities ORDER BY entity_id").fetchall() == [
+            (1, "Berlin", "LOC"),
+            (2, "Germany", "LOC"),
+            (3, "Albert Einstein", "PER"),
+            (4, "Ulm", "LOC"),
+            (5, "Amazon", "ORG"),
+            (6, "Jeff Bezos", "PER"),
+        ]
+
+    def test_relations(self, knowledge_base: sqlite3.Cursor) -> None:
+        assert knowledge_base.execute("SELECT * FROM relations ORDER BY relation_id").fetchall() == [
+            (1, 1, 2, "capital_of"),  # Berlin -> Germany
+            (2, 3, 4, "born_in"),  # Albert Einstein -> Ulm
+            (3, 4, 2, "located_in"),  # Ulm -> Germany
+            (4, 5, 6, "founded_by"),  # Amazon -> Jeff Bezos
+        ]
+
+    def test_sentence_entities(self, knowledge_base: sqlite3.Cursor) -> None:
+        assert knowledge_base.execute("SELECT * FROM sentence_entities ORDER BY sentence_entity_id").fetchall() == [
+            # Sentence 1: "Berlin is the capital of Germany."
+            (1, 1, 1, 1.0),  # Berlin
+            (2, 1, 2, 1.0),  # Germany,
+            # Sentence 2: "Berlin is the capital of Germany."
+            (3, 2, 1, 1.0),  # Berlin
+            (4, 2, 2, 1.0),  # Germany
+            # Sentence 3: "Albert Einstein was born in Ulm, Germany."
+            (5, 3, 3, 1.0),  # Albert Einstein
+            (6, 3, 4, 1.0),  # Ulm
+            (7, 3, 2, 1.0),  # Germany
+            # Sentence 4: "Ulm, located in Germany, is the birthplace of Albert Einstein."
+            (8, 4, 4, 1.0),  # Ulm
+            (9, 4, 2, 1.0),  # Germany
+            (10, 4, 3, 1.0),  # Albert Einstein
+            # Sentence 5: "Amazon was founded by Jeff Bezos."
+            (11, 5, 5, 1.0),  # Amazon
+            (12, 5, 6, 1.0),  # Jeff Bezos
+        ]
+
+    def test_sentence_relations(self, knowledge_base: sqlite3.Cursor) -> None:
+        assert knowledge_base.execute("SELECT * FROM sentence_relations ORDER BY sentence_relation_id").fetchall() == [
+            # Sentence 1: "Berlin is the capital of Germany."
+            (1, 1, 1, 1.0),  # Berlin -> Germany
+            # Sentence 2: "Berlin is the capital of Germany."
+            (2, 2, 1, 1.0),  # Berlin -> Germany
+            # Sentence 3: "Albert Einstein was born in Ulm, Germany."
+            (3, 3, 2, 1.0),  # Albert Einstein -> Ulm
+            (4, 3, 3, 1.0),  # Ulm -> Germany
+            # Sentence 4: "Ulm, located in Germany, is the birthplace of Albert Einstein."
+            (5, 4, 2, 1.0),  # Albert Einstein -> Ulm
+            (6, 4, 3, 1.0),  # Ulm -> Germany
+            # Sentence 5: "Amazon was founded by Jeff Bezos."
+            (7, 5, 4, 1.0),  # Amazon -> Jeff Bezos
+        ]
 
 
-def test_entities(knowledge_base: sqlite3.Cursor) -> None:
-    assert knowledge_base.execute("SELECT * FROM entities ORDER BY entity_id").fetchall() == [
-        (1, "Berlin", "LOC"),
-        (2, "Germany", "LOC"),
-        (3, "Albert Einstein", "PER"),
-        (4, "Ulm", "LOC"),
-        (5, "Amazon", "ORG"),
-        (6, "Jeff Bezos", "PER"),
-    ]
+class TestRelationMetrics:
+    def test_occurrence(self, knowledge_base: sqlite3.Cursor) -> None:
+        assert knowledge_base.execute(
+            "SELECT relation_id, occurrence FROM relation_metrics ORDER BY relation_id"
+        ).fetchall() == [
+            (1, 2),  # Berlin          ---capital_of--> Germany
+            (2, 2),  # Albert Einstein ---born_in-----> Ulm
+            (3, 2),  # Ulm             ---located_in--> Germany
+            (4, 1),  # Amazon          ---founded_by--> Jeff
+        ]
 
+    def test_distinct_occurrence(self, knowledge_base: sqlite3.Cursor) -> None:
+        assert knowledge_base.execute(
+            "SELECT relation_id, total_occurrence FROM relation_metrics ORDER BY relation_id"
+        ).fetchall() == [
+            (1, 1),  # Berlin          ---capital_of--> Germany
+            (2, 2),  # Albert Einstein ---born_in-----> Ulm
+            (3, 2),  # Ulm             ---located_in--> Germany
+            (4, 1),  # Amazon          ---founded_by--> Jeff
+        ]
 
-def test_relations(knowledge_base: sqlite3.Cursor) -> None:
-    assert knowledge_base.execute("SELECT * FROM relations ORDER BY relation_id").fetchall() == [
-        (1, 1, 2, "capital_of"),  # Berlin -> Germany
-        (2, 3, 4, "born_in"),  # Albert Einstein -> Ulm
-        (3, 4, 2, "located_in"),  # Ulm -> Germany
-        (4, 5, 6, "founded_by"),  # Amazon -> Jeff Bezos
-    ]
-
-
-def test_sentence_entities(knowledge_base: sqlite3.Cursor) -> None:
-    assert knowledge_base.execute("SELECT * FROM sentence_entities ORDER BY sentence_entity_id").fetchall() == [
-        # Sentence 1: "Berlin is the capital of Germany."
-        (1, 1, 1, 1.0),  # Berlin
-        (2, 1, 2, 1.0),  # Germany,
-        # Sentence 2: "Berlin is the capital of Germany."
-        (3, 2, 1, 1.0),  # Berlin
-        (4, 2, 2, 1.0),  # Germany
-        # Sentence 3: "Albert Einstein was born in Ulm, Germany."
-        (5, 3, 3, 1.0),  # Albert Einstein
-        (6, 3, 4, 1.0),  # Ulm
-        (7, 3, 2, 1.0),  # Germany
-        # Sentence 4: "Ulm, located in Germany, is the birthplace of Albert Einstein."
-        (8, 4, 4, 1.0),  # Ulm
-        (9, 4, 2, 1.0),  # Germany
-        (10, 4, 3, 1.0),  # Albert Einstein
-        # Sentence 5: "Amazon was founded by Jeff Bezos."
-        (11, 5, 5, 1.0),  # Amazon
-        (12, 5, 6, 1.0),  # Jeff Bezos
-    ]
-
-
-def test_sentence_relations(knowledge_base: sqlite3.Cursor) -> None:
-    assert knowledge_base.execute("SELECT * FROM sentence_relations ORDER BY sentence_relation_id").fetchall() == [
-        # Sentence 1: "Berlin is the capital of Germany."
-        (1, 1, 1, 1.0),  # Berlin -> Germany
-        # Sentence 2: "Berlin is the capital of Germany."
-        (2, 2, 1, 1.0),  # Berlin -> Germany
-        # Sentence 3: "Albert Einstein was born in Ulm, Germany."
-        (3, 3, 2, 1.0),  # Albert Einstein -> Ulm
-        (4, 3, 3, 1.0),  # Ulm -> Germany
-        # Sentence 4: "Ulm, located in Germany, is the birthplace of Albert Einstein."
-        (5, 4, 2, 1.0),  # Albert Einstein -> Ulm
-        (6, 4, 3, 1.0),  # Ulm -> Germany
-        # Sentence 5: "Amazon was founded by Jeff Bezos."
-        (7, 5, 4, 1.0),  # Amazon -> Jeff Bezos
-    ]
-
-
-def test_relation_metrics(knowledge_base: sqlite3.Cursor) -> None:
-    # | relation_id | occurrence | distinct_occurrence |
-    assert knowledge_base.execute("SELECT * FROM relation_metrics ORDER BY relation_id").fetchall() == [
-        (1, 2, 1),  # Berlin          ---capital_of--> Germany
-        (2, 2, 2),  # Albert Einstein ---born_in-----> Ulm
-        (3, 2, 2),  # Ulm             ---located_in--> Germany
-        (4, 1, 1),  # Amazon          ---founded_by--> Jeff
-    ]
+    def test_entropy(self) -> None:
+        pass
 
 
 def test_relation_overview(knowledge_base: sqlite3.Cursor) -> None:
