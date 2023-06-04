da875e71818362bfd88cc153ba7a7db1e075d877
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-06-04 22:39:03 +0200

    Add `none_or_str` type

--- src/selfrel/utils/argparse.py
+++ src/selfrel/utils/argparse.py
@@ -1,5 +1,12 @@
 import argparse
+from typing import Optional
+
+__all__ = ["RawTextArgumentDefaultsHelpFormatter", "none_or_str"]
 
 
 class RawTextArgumentDefaultsHelpFormatter(argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
     pass
+
+
+def none_or_str(value: str) -> Optional[str]:
+    return None if value == "None" else value
